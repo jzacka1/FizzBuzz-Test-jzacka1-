203b718e37fb775d2b3f48415b5254695d9d542a
JZacka1 <30647180+jzacka1@users.noreply.github.com>
2017-09-19 11:43:42 -0400

    Add files via upload

---
+++ FizzBuzz Test/FizzBuzz Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that traverses through a list of numbers from 1 -100.
+            //If a number is divisible by 3, Print out "Fizz".
+            //If a number is divisible by 5, Print out "Buzz".
+            //If neither of the two conditions match, print the number.
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+
+        }
+    }
+}
