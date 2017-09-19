3f75c45a7ffe97a64f13bd36794c3a213903db98
jzacka1 <jameszacka@gmail.com>
2017-09-19 18:27:42 -0400

    Added additional conditional to display "FizzBuzz" message.

--- FizzBuzz Test/FizzBuzz Test/Program.cs
+++ FizzBuzz Test/FizzBuzz Test/Program.cs
@@ -16,7 +16,9 @@ namespace FizzBuzz_Test
             //If neither of the two conditions match, print the number.
             for (int i = 0; i <= 100; i++)
             {
-                if (i % 3 == 0)
+                if(i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 3 == 0)
                     Console.WriteLine("Fizz");
                 else if (i % 5 == 0)
                     Console.WriteLine("Buzz");
